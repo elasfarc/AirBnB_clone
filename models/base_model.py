ada834d5fe8c76c4d15fdc4904db4a7800eb073d
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-12-06 10:13:09 +0200

    [BaseModel] Add to_dict && __str__ public instance methods

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ module containing the class BaseModel
 
 import uuid
 from datetime import datetime
+from typing import Any, Dict, TypedDict
 
 
 class BaseModel:
@@ -36,3 +37,53 @@ class BaseModel:
         refreshing the 'updated_at' timestamp to the current date and time.
         """
         self.updated_at = datetime.now()
+
+    def to_dict(self) -> Dict[str, Any]:
+        """
+        to_dict - Converts the object to a dictionary.
+
+        Description:
+        This method converts the object to a dictionary representation.
+        It iterates through the instance's attributes,
+        handling timestamp keys by converting their values to ISO format using
+        the `to_iso_format` function. The resulting dictionary includes all
+        instance attributes and the class name.
+
+        Return:
+        A dictionary representation of the object,
+        where timestamp values are converted to ISO format.
+
+        """
+        def to_iso_format(dt: datetime) -> str:
+            """
+            to_iso_format - Converts a datetime object to ISO format.
+
+            Parameters:
+            - dt: A datetime object.
+
+            Return:
+            A string representing the ISO format of the input datetime.
+
+            """
+            return dt.isoformat()
+        timestamp_keys = ["created_at", "updated_at"]
+
+        obj_dict = {
+            key: value if key not in timestamp_keys else to_iso_format(value)
+            for key, value in self.__dict__.items()
+        }
+
+        obj_dict.update(__class__=self.__class__.__name__)
+
+        return obj_dict
+
+    def __str__(self):
+        """
+        __str__ - Returns a string representation of the object.
+
+        Return:
+        A string representing the object, formatted as
+        "[ClassName] (ObjectID) {__dict__}".
+        """
+        cls_name = self.__class__.__name__
+        return f"[{cls_name}] ({self.id}) {self.__dict__}"
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,7 +4,7 @@ module containing tests for the BaseModel class.
 
 import unittest
 from unittest.mock import patch
-from datetime import timedelta
+from datetime import timedelta, datetime
 from models.base_model import BaseModel
 
 
@@ -77,3 +77,127 @@ class TestBaseModel(unittest.TestCase):
 
         self.assertNotEqual(base.updated_at, current_updated)
         self.assertGreater(base.updated_at, current_updated)
+
+    def test_string_representation_with_class_name_id_and_attributes(self):
+        """
+        test_string_representation_with_class_name_id_and_attributes
+            Test the string representation with class name, ID,
+            and attributes.
+
+        Description:
+        This test case creates an instance of the BaseModel class,
+        retrieves its string representation, and compares it
+        with the expected string.
+        The expected string is formatted as "[BaseModel] (ObjectID) {__dict__}"
+
+        """
+        obj = BaseModel()
+        expected = f"[BaseModel] ({obj.id}) {obj.__dict__}"
+        self.assertEqual(str(obj), expected)
+
+    #  The string representation includes all attributes of the object.
+    def test_string_representation_includes_all_attributes(self):
+        """
+        test_string_representation_includes_all_attributes
+            Test that the string representation includes all attributes.
+
+        Description:
+        This test case defines a TestModel class that inherits from BaseModel,
+        initializes some attributes, creates an instance, retrieves its string
+        representation, and compares it with the expected string.
+        The expected string is formatted as "[TestModel] (ObjectID) {__dict__}"
+        """
+        class TestModel(BaseModel):
+            """
+            TestModel - A subclass of BaseModel for testing purposes.
+            """
+            def __init__(self):
+                super().__init__()
+                self.attribute1 = "value1"
+                self.attribute2 = "value2"
+
+        obj = TestModel()
+        expected = f"[TestModel] ({obj.id}) {obj.__dict__}"
+        self.assertEqual(str(obj), expected)
+
+
+class TestDictionaryConversion(unittest.TestCase):
+    """
+    TestDictionaryConversion - Test case for the dictionary conversion
+    methods in the BaseModel class.
+    """
+    def test_timestamps(self):
+        """
+        test_timestamps - Test handling of timestamps in the to_dict method.
+
+        Description:
+        This test method uses the unittest.TestCase `patch` functionality
+        to mock the datetime module, setting a fixed date for testing.
+        It creates an instance of the BaseModel class, converts it to
+        a dictionary, and then asserts that the timestamp attributes in the
+        resulting dictionary are of type string and have the expected values.
+
+        """
+        with (patch("models.base_model.datetime") as mock_datetime):
+            mock_datetime.now.return_value =\
+                datetime.fromisoformat("2024-01-01")
+            obj = BaseModel()
+            dic = obj.to_dict()
+
+        self.assertIsInstance(dic["created_at"], str)
+        self.assertIsInstance(dic["updated_at"], str)
+        self.assertEqual(dic["created_at"], obj.created_at.isoformat())
+        self.assertEqual(dic["created_at"], obj.created_at.isoformat())
+        # expected =
+
+    def test_returns_dictionary_with_attributes_and_cls_name(self):
+        """
+        test_returns_dictionary_with_attributes_and_cls_name
+            Test that to_dict returns a dictionary with attributes and
+            class name.
+
+        Description:
+        This test method creates an instance of the BaseModel class,
+        converts it to a dictionary, and asserts that the resulting
+        dictionary includes attributes such as '__class__', 'id', 'created_at',
+        and 'updated_at', with correct values.
+
+        """
+        obj = BaseModel()
+        dic = obj.to_dict()
+
+        self.assertIsInstance(dic, dict)
+        self.assertEqual(dic["__class__"], obj.__class__.__name__)
+        self.assertEqual(dic["id"], obj.id)
+        self.assertEqual(dic["created_at"], obj.created_at.isoformat())
+        self.assertEqual(dic["created_at"], obj.created_at.isoformat())
+
+    def test_works_with_attributes_of_different_types(self):
+        """
+        test_works_with_attributes_of_different_types
+            Test that to_dict works with attributes of different types.
+
+        Description:
+        This test method defines a TestModel class that inherits from BaseModel
+        and initializes attributes of various types.
+        It creates an instance of TestModel, converts it to a dictionary,
+        and asserts that the resulting dictionary contains attributes with
+        their correct values.
+
+        """
+        class TestModel(BaseModel):
+            """
+            TestModel - A subclass of BaseModel for testing purposes.
+            """
+            def __init__(self):
+                super().__init__()
+                self.name = "Test"
+                self.age = 20
+                self.is_active = True
+
+        obj = TestModel()
+        dic = obj.to_dict()
+
+        self.assertEqual(dic["name"], obj.name)
+        self.assertEqual(dic["age"], obj.age)
+        self.assertEqual(dic["is_active"], obj.is_active)
