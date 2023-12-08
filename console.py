0c698aa8c3965f52351f4040a7329084d444eec6
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-12-09 01:14:35 +0200

    [console.py] Add 'create' command to HBNBCommand class

--- console.py
+++ console.py
@@ -3,6 +3,9 @@
 
 import cmd
 from textwrap import dedent
+from models.base_model import BaseModel
+from models.virtual import JsonStorableEntity
+from typing import Dict, Type
 
 
 def format_docstring(fn):
@@ -20,13 +23,32 @@ def format_docstring(fn):
 class HBNBCommand(cmd.Cmd):
     """ HBNB console commands and helper functions """
 
+    __classes: Dict[str, Type[JsonStorableEntity]] = {
+        'BaseModel': BaseModel
+    }
+
     prompt = "(hbnb) "
 
     @format_docstring
+    def do_create(self, s):
+        """
+        create command - Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id
+        ex: create [className]
+        """
+        args = s.split()
+        if len(args) < 1:
+            print("** class name missing **")
+        elif (cls_name := args[0]) not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            obj = self.__classes[cls_name]()
+            obj.save()
+            print(obj.id)
+
+    @format_docstring
     def do_quit(self, _):
-        """
-        Quit command to exit the program
-        """
+        """Quit command to exit the program"""
         return True
 
     @format_docstring
