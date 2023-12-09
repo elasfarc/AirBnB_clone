2846a76138c71af8544a76bf81236817b9e9bad9
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-12-09 02:47:30 +0200

    [console.py] Add 'show' command

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ import cmd
 from textwrap import dedent
 from models.base_model import BaseModel
 from models.virtual import JsonStorableEntity
+from models import storage
 from typing import Dict, Type
 
 
@@ -30,7 +31,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     @format_docstring
-    def do_create(self, s):
+    def do_create(self, s: str):
         """
         create command - Creates a new instance of BaseModel,
         saves it (to the JSON file) and prints the id
@@ -45,6 +46,26 @@ class HBNBCommand(cmd.Cmd):
             obj = self.__classes[cls_name]()
             obj.save()
             print(obj.id)
+
+    @format_docstring
+    def do_show(self, s: str):
+        """
+        show command - Prints the string representation of an instance
+        based on the class name and id. Ex: $ show [className] [object_id].
+        """
+        args = s.split()
+        if len(args) < 1:
+            print("** class name missing **")
+        elif (cls_name := args[0]) not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            obj_id = args[1]
+            key = f"{cls_name}.{obj_id}"
+            objects = storage.all()
+            print("** no instance found **" if key not in objects else
+                  f"{objects[key]}")
 
     @format_docstring
     def do_quit(self, _):
